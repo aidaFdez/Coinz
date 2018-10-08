cd57f5c9e3679c46522b31033fda9813a1417ac2
aida <33495958+aidaFdez@users.noreply.github.com>
2018-10-08 10:20:44 +0100

    Added menu and bank activities with buttons

--- Coinz/app/src/main/java/com/example/aafo/coinz/MainActivity.java
+++ Coinz/app/src/main/java/com/example/aafo/coinz/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton menu = (FloatingActionButton) findViewById(R.id.menu);
+        menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -48,5 +48,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void goToMenu (View view){
+
     }
 }
