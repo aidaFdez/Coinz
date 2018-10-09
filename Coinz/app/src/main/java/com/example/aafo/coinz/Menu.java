1b7ff1f2bd3f7f2df989657dcb7d2db914c4798f
aidaFdez <S1dn3131>
2018-10-09 16:26:06 +0100

    Finished the navigational buttons. Also added back the floating button, which opens the menu.

--- Coinz/app/src/main/java/com/example/aafo/coinz/Menu.java
+++ Coinz/app/src/main/java/com/example/aafo/coinz/Menu.java
@@ -1,7 +1,9 @@
 package com.example.aafo.coinz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+
+    public void goToBank (View view){
+        Intent intBank = new Intent(Menu.this, Bank.class);
+        startActivity(intBank);
+    }
+
+    public void goToMedals (View view){
+        Intent intMed = new Intent(Menu.this, Medals.class);
+        startActivity(intMed);
+    }
+
+    public void goToNews (View view){
+        Intent intNews = new Intent(Menu.this, News.class);
+        startActivity(intNews);
+    }
+
+    public void goToSend (View view){
+        Intent intSend = new Intent(Menu.this, SendFriends.class);
+        startActivity(intSend);
     }
 }
