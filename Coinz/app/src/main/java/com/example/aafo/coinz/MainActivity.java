e2d7b11dcfe3af29fc784770cdec661c3c774a8f
aidaFdez <S1dn3131>
2018-10-19 14:03:14 +0100

    The map shows up now in the main activity. I had to add compileOptions to the Gradle to make it work.

--- Coinz/app/src/main/java/com/example/aafo/coinz/MainActivity.java
+++ Coinz/app/src/main/java/com/example/aafo/coinz/MainActivity.java
@@ -4,17 +4,66 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.content.Intent;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.Mapbox;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mapView.onDestroy();
     }
 
     public void goToMenu (View view){
         Intent intMenu = new Intent(MainActivity.this, Menu.class);
         startActivity(intMenu);
     }
+
+    private MapView mapView;
 }
